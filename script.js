ba0cd35304111dd93b9854bc0e694643bfc27f0e
Ketevan12 <kirvk209@newschool.edu>
2023-03-28 00:48:17 -0400

    work in progress

--- script.js
+++ script.js
@@ -1,62 +1,30 @@
-script.js
 
-// Select the text you want to make it circular
-const text = document.querySelector(".circular-text .text")
+const CirclesList = document.getElementById('Circles')
 
-// Make the selected text circler with CircleType
-// you can find the full docs here: https://circletype.labwire.ca/
-const rotate = new CircleType(text).radius(65)
-
-// Add a scroll listener to the window object and rotate the selected text according to the scroll
-// we used * 0.15 to make the rotation looks smoother
-window.addEventListener("scroll", function(){
-  text.style.transform=`rotate(${window.scrollY * 0.15}deg)`
-})
-
-URL url = new URL("https://docs.google.com/spreadsheets/d/1KQfuwrevX2LYqBn2u6c91u3ctMTpi46ajUCtZ7Z2klI/edit?usp=sharing.json");
-HttpURLConnection con = (HttpURLConnection) url.openConnection();
-con.setRequestMethod("GET");
-
-BufferedReader in = new BufferedReader(
-    new InputStreamReader(con.getInputStream()));
-
-String inputLine;
-StringBuilder response = new StringBuilder();
-while ((inputLine = in.readLine()) != null) {
-    response.append(inputLine);
-}
-in.close();
-
-String json = response.toString();
-
-InputStream inputStream = getClass().getClassLoader().getResourceAsStream("resources.json");
-
-const albumsList = document.getElementById('albums-list')
-
-function renderCircle(Circle, i) {
+function renderCircles(Circles, i) {
   // Create .Circle element
-  const CircleElement = document.createElement('div')
+  const CirclesElement = document.createElement('div')
 
-  CircleElement.classList.add('Circle')
-  CircleElement.classList.add(`Circle-${i}`)
+  CirclesElement.classList.add('Circles')
+  CirclesElement.classList.add(`Circles-${i}`)
 
-  console.log(CircleElement)
-  console.log(Circle.cover_image_url)
+  console.log(CirclesElement)
+  console.log(CirclesHTML.cover_image_url)
 
   // Create the inner HTML content
-  const CircleHTML = `
-    <a href="CircleDetail.html?title=${Circle.number}">
-      <img src="${Circle.gradiant}">
+  const CirclesHTML = `
+    <a href="CircleDetail.html?title=${Circles.number}">
+      <img src="${Circles.gradiant}">
     </a>
     <ul>
-      <li>Circle Number: ${Circle.}</li>
-      <li>Ambient Sound: ${Circle.}</li>
-      <li>Transcript: ${Circle.}</li>
-      <li>Connection: ${Circle.}</li>
-      <li>Duration: ${Circle.}</li>
-      <li>Association: ${Circle.}</li>
+      <li>Circle-Number: ${Circles.}</li>
+      <li>Ambient-Sound: ${Circles.}</li>
+      <li>Transcript: ${Circles.}</li>
+      <li>Connection: ${Circles.}</li>
+      <li>Duration: ${Circles.}</li>
+      <li>Association: ${Circles.}</li>
       <li>
-        <a href="${Circle.number}">
+        <a href="${Circles.number}">
          Listen Here
         </a>
       <li>
@@ -64,25 +32,24 @@ function renderCircle(Circle, i) {
   `
 
   // Set the inner HTML content of the albumElement
-  CircleElement.innerHTML = CircleHTML
+  CirclesElement.innerHTML = CirclesHTML
 
   // Add to the list #albums-list
-  CircleList.appendChild(CircleElement)
+  CirclesList.appendChild(CirclesElement)
 }
 
-// Start App!
-// albums.forEach(function(album) {
-//   // if (album.year_released < 2000) {
-//     renderAlbum(album)
-//   // }
-// })
+Circles.forEach(function(Circles) {
+ if (Circles.Circle-number < 2000) {
+    renderCircles(Circle)
+ } })
 
 fetch('Circles.json')
     .then((response) => response.json())
     .then((json) => {
-      json.albums.forEach(function(album, i) {
+      json.Circles.forEach(function(Circles, i) {
         // if (album.year_released < 2000) {
-          renderCircles(Circle, i)
+          renderCircles(Circles, i)
         // }
       })
     });
+
